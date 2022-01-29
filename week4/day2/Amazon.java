47cb493a7f7ff0d7706e0927ff0ffd1291020c9f
vengateshharvey <96675621+vengateshharvey@users.noreply.github.com>
2022-01-29 12:11:02 +0530

    wee4day2

---
+++ week4/day2/Amazon.java
@@ -0,0 +1,10 @@
+package week4.day2;
+
+public class Amazon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ week4/day2/Draggable.java
@@ -0,0 +1,10 @@
+package week4.day2;
+
+public class Draggable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
