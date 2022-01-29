9e8d0e5020d09869cfaecae42483ebe3ae6556d8
vengateshharvey <96675621+vengateshharvey@users.noreply.github.com>
2022-01-29 12:08:31 +0530

    week4day1

---
+++ week4/day1/AlertFrame.java
@@ -0,0 +1,10 @@
+package week4.day1;
+
+public class AlertFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ week4/day1/Learnframe.java
@@ -0,0 +1,10 @@
+package week4.day1;
+
+public class Learnframe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
